c15b0bf55667b64e66912ca3cf29858316a14dcd
Asger <asgerh@docker01.hulgaard.eu>
2025-11-12 20:23:53 +0100

    Add web UI CSS bundles and update project configuration
    - Added scoped CSS bundles for AdminPanel, ItemEditor, and Map components
    - Included compressed versions (.br and .gz) for optimized delivery
    - Updated Startup.Server project configuration with proper build settings and dependencies
    - Normalized line endings for documentation files (CRLF to LF)

--- src/Web/AdminPanel/Pages/EditAccount.razor.cs
+++ src/Web/AdminPanel/Pages/EditAccount.razor.cs
@@ -4,6 +4,7 @@
 
 namespace MUnique.OpenMU.Web.AdminPanel.Pages;
 
+using System.Collections;
 using System.Threading;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
@@ -20,6 +21,8 @@ using MUnique.OpenMU.Web.AdminPanel.Components.ItemEdit;
 [Route("/edit-account/{accountId:guid}/{typeString}/{id:guid}")]
 public partial class EditAccount : EditBase
 {
+    private AccountDataSourceWrapper? _dataSourceWrapper;
+
     /// <summary>
     /// Gets or sets the identifier of the account which should be edited.
     /// </summary>
@@ -33,7 +36,7 @@ public partial class EditAccount : EditBase
     public IDataSource<Account> AccountData { get; set; } = null!;
 
     /// <inheritdoc />
-    protected override IDataSource EditDataSource => this.AccountData;
+    protected override IDataSource EditDataSource => this._dataSourceWrapper ??= new AccountDataSourceWrapper(this.AccountData, this.AccountId);
 
     /// <inheritdoc />
     protected override async ValueTask LoadOwnerAsync(CancellationToken cancellationToken)
@@ -71,6 +74,122 @@ public partial class EditAccount : EditBase
             builder.AddAttribute(++currentSequence, nameof(AutoForm<object>.Model), this.Model);
             builder.AddAttribute(++currentSequence, nameof(AutoForm<object>.OnValidSubmit), EventCallback.Factory.Create(this, this.SaveChangesAsync));
             builder.CloseComponent();
+        }
+    }
+
+    /// <summary>
+    /// Wrapper for AccountData that also supports Character and Item types by loading them from the Account.
+    /// </summary>
+    private class AccountDataSourceWrapper : IDataSource
+    {
+        private readonly IDataSource<Account> _accountData;
+        private readonly Guid _accountId;
+
+        public AccountDataSourceWrapper(IDataSource<Account> accountData, Guid accountId)
+        {
+            this._accountData = accountData;
+            this._accountId = accountId;
+        }
+
+        public bool IsSupporting(Type type) =>
+            type == typeof(Account) || type == typeof(Character) || type == typeof(Item);
+
+        public IIdentifiable? Get(Guid id)
+        {
+            var account = this._accountData.Get(this._accountId) as Account;
+            if (account is null)
+            {
+                return null;
+            }
+
+            // Check if the requested ID is the account itself
+            if (account.GetId() == id)
+            {
+                return account as IIdentifiable;
+            }
+
+            // Try to find a character with this ID
+            var character = account.Characters.FirstOrDefault(c => c.GetId() == id);
+            if (character is not null)
+            {
+                return character as IIdentifiable;
+            }
+
+            // Try to find an item with this ID
+            var item = account.Vault?.Items.FirstOrDefault(i => i.GetId() == id);
+            return item as IIdentifiable;
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            if (typeof(T) == typeof(Account))
+            {
+                return this._accountData.GetAll<T>();
+            }
+
+            var account = this._accountData.Get(this._accountId) as Account;
+            if (account is null)
+            {
+                return Array.Empty<T>();
+            }
+
+            if (typeof(T) == typeof(Character))
+            {
+                return account.Characters.OfType<T>();
+            }
+
+            if (typeof(T) == typeof(Item) && account.Vault is not null)
+            {
+                return account.Vault.Items.OfType<T>();
+            }
+
+            return Array.Empty<T>();
+        }
+
+        public IEnumerable GetAll(Type type)
+        {
+            if (type == typeof(Account))
+            {
+                return this._accountData.GetAll(type);
+            }
+
+            var account = this._accountData.Get(this._accountId) as Account;
+            if (account is null)
+            {
+                return Array.Empty<object>();
+            }
+
+            if (type == typeof(Character))
+            {
+                return account.Characters;
+            }
+
+            if (type == typeof(Item) && account.Vault is not null)
+            {
+                return account.Vault.Items;
+            }
+
+            return Array.Empty<object>();
+        }
+
+        public async ValueTask<IContext> GetContextAsync(CancellationToken cancellationToken = default)
+        {
+            return await this._accountData.GetContextAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public async ValueTask<object> GetOwnerAsync(Guid ownerId = default, CancellationToken cancellationToken = default)
+        {
+            return (await this._accountData.GetOwnerAsync(ownerId, cancellationToken).ConfigureAwait(false))!;
+        }
+
+        public async ValueTask DiscardChangesAsync()
+        {
+            await this._accountData.DiscardChangesAsync().ConfigureAwait(false);
+        }
+
+        public void Dispose()
+        {
+            // The underlying account data source is managed by DI, so we don't dispose it here
         }
     }
 }
